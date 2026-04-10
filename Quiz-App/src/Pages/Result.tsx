6d10b5572fba1ae43d641bbb397c9f447a781b0b
Deng Kuat <denq1738@gmail.com>
2026-04-10 22:34:06 +0200

    UPDATE: A button to play again when clicked we reset the score back to 0

--- Quiz-App/src/Pages/Result.tsx
+++ Quiz-App/src/Pages/Result.tsx
@@ -1,24 +1,47 @@
 import { useNavigate } from "react-router-dom"
+import { useResult } from "../Context/useContext";
 
 export default function Result() {
   const navigate = useNavigate()
+  const {result, setResult} = useResult();
 
   const handleNavigation = () => {
     navigate("/QuestionTemplate/1")
+    setResult(0)
+  }
+
+  const handleNavigationExit = () => {
+    navigate("/")
   }
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div className="border py-40 px-100 rounded-2xl shadow-2xl">
-        <div>Result</div>
-        <div>
+    <div className="h-screen flex justify-evenly items-center flex-col">
+      
+        {/* user stats  */}
+
+        <div className="text-center space-y-7">
+          <p className="text-3xl font-bold">Final Score</p>
+          <p className="text-2xl">{result}/10</p>
+          {result> 5 ? 
+          (<p className="text-2xl">Excellent, keep it up</p>)
+          :
+          (<p className="text-2xl pt-7">Fair, but try better next time</p>)}
+        </div>
+
+        <div className="space-x-20">
           <button
           className="border rounded-2xl py-2 px-6 bg-blue-800 text-white hover:bg-blue-900 duration-300"
           onClick={() => handleNavigation()}
           >
             Play Again
           </button>
+
+          <button
+          className="border rounded-2xl py-2 px-6 bg-blue-800 text-white hover:bg-blue-900 duration-300"
+          onClick={() => handleNavigationExit()}
+          >
+            Instruction
+          </button>
         </div>
-      </div>
     </div>
   )
 }
