3801bb4bbf94214285c0df468becbe1d1bd83c17
Deng Kuat <denq1738@gmail.com>
2026-04-10 22:37:14 +0200

    UPDATE: When right answer is clicked, the result by the setter function result adds one to the score

--- Quiz-App/src/Pages/QuestionTemplate.tsx
+++ Quiz-App/src/Pages/QuestionTemplate.tsx
@@ -1,38 +1,86 @@
+import { useState } from "react";
 import { questions } from "../components/Questions"
 import { useParams, useNavigate } from "react-router-dom"
+import { useResult } from "../Context/useContext";
 
-// interface questionAndOptionInterface {
-//   id:number;
-//   question:string;
-//   options:string[]
-// }
 
 export const QuestionTemplate = () => {
-  const {id} = useParams();
+  const [click, setClick] = useState<boolean>(false)
+  const { id } = useParams();
   const navigation = useNavigate()
+  const question = questions[Number(id) - 1]
+  const answers = question.options
+  const {setResult} = useResult()
 
   const handleNavigation = () => {
-    if(Number(id)+1 <= 10){
-      return navigation(`/QuestionTemplate/${Number(id)+1}`)
+    setClick(false)
+    if (Number(id) + 1 <= 10) {
+      return navigation(`/QuestionTemplate/${Number(id) + 1}`)
+    }
+    return navigation(`/result`)
+  } 
+
+  const handleCorrect = () => {
+    setClick(false)
+    if(!click) return
+
+    if (Number(id) + 1 <= 10) {
+      return navigation(`/QuestionTemplate/${Number(id) + 1}`)
     }
     return navigation(`/result`)
   }
+
+  const handleRightAnswerSum = (index:number) =>{
+    setClick(true)
+    if(index === question.correct){
+    setResult((prev) => prev+1)
+    }
+  }
+console.log(answers)
+
   return (
     <div>
-      <div className="h-screen flex justify-center items-center">
-        <div className="border py-40 px-100 rounded-2xl shadow-2xl">
-          <h1>The id of here is {Number(id)+ 1}</h1>
+      <div className="h-screen flex justify-evenly items-center flex-col">
 
-          <div>
-            <button 
-            className="border rounded-2xl px-9 py-2"
+        <div className="text-3xl font-bold">
+          <h1>Question {id} out of 10</h1>
+        </div>
+
+        <div className="text-3xl font-semibold">
+          <p>{question.question}</p>
+
+        </div>
+
+        <div className="grid grid-cols-2 gap-10 text-2xl cursor-pointer">
+          {answers.map((solution, index) => (
+            <p
+            key={index}
+            onClick={() => handleRightAnswerSum(index)}
+            className={click ? index === question.correct ?
+              "text-green-700"
+              :
+              "text-red-700":"text-black"}
+            >{index + 1}.  {solution}</p>
+          ))}
+        </div>
+
+        <div className="flex justify-end pt-6 space-x-20">
+          <button
+            className="border rounded-2xl py-2 px-6 bg-blue-800 text-white hover:bg-blue-900 duration-300 "
+            onClick={() => handleCorrect()}
+          >
+            Next Question
+          </button>
+
+          <button
+            className="border rounded-2xl py-2 px-16 bg-blue-800 text-white hover:bg-blue-900 duration-300 "
             onClick={() => handleNavigation()}
-            >
-              Next Question
-            </button>
-          </div>
+          >
+            Skip
+          </button>
         </div>
       </div>
+
     </div>
   )
 }
