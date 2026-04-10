744a05bb178a603464f50b57bf3b7a1586f82f0d
Deng Kuat <denq1738@gmail.com>
2026-04-10 21:38:23 +0200

    UPDATE: Wrapped the <App/> inside the contextProvier for all components to use the global state

--- Quiz-App/src/main.tsx
+++ Quiz-App/src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { ResultProvider } from './Context/Context.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ResultProvider>
+        <App />
+      </ResultProvider>
+
     </BrowserRouter>
 
   </StrictMode>,
