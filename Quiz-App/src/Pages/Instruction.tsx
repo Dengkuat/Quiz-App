85a130e952547095508fb66fbb8f3d7695afcf38
Deng Kuat <denq1738@gmail.com>
2026-04-10 19:49:55 +0200

    UPDATE:Create a button that programmically navigates us to questions section from question 1

---
+++ Quiz-App/src/Pages/Instruction.tsx
@@ -0,0 +1,37 @@
+import { useNavigate} from "react-router-dom"
+
+export default function UserInstruction() {
+  const navigate = useNavigate();
+
+  const handleNavigation = () => {
+    navigate("/QuestionTemplate/1")
+  }
+  return (
+    <div>
+    <div className="h-screen flex justify-center items-center">
+      <div className="border py-30 px-20 rounded-2xl shadow-2xl space-y-10">
+        <div className="space-y-4">
+          <h1 className="text-center pb-7 font-bold text-4xl">Welcome to the Quiz App</h1>
+          <h2 className="text-center pb-7 font-bold text-2xl">Before you begin, please read the instructions carefully:</h2>
+          <p>	1.	This quiz contains 10 questions in total.
+          You will answer them one by one.</p>
+          <p>3.	When you select an option, the app will immediately show you the correct answer.
+          Your score will also update automatically based on your selection.</p>
+          <p>	4.	You cannot change your answer once it has been selected, so choose carefully.</p>
+          <p>	5.	After answering a question, click the “Next” button to move to the next question.</p>
+          <p>	6.	Your progress will be tracked throughout the quiz, and your final score will be shown at the end.</p>
+        </div>
+
+        <div className="flex justify-center items-center">
+          <button
+          className="border rounded-2xl py-2 px-6 bg-blue-800 text-white hover:bg-blue-900 duration-300"
+          onClick={() => handleNavigation()}
+          >
+            Start Quiz
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
